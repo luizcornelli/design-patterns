17de6f867dd9d18dd5fe87d0dd994da69a3a53be
Luiz Cornelli <luizcornelli.dev@gmail.com>
2022-01-19 20:24:32 -0300

    Bridge

---
+++ src/estruturais/bridge/AdvancedRemote.java
@@ -0,0 +1,16 @@
+package estruturais.bridge;
+
+public class AdvancedRemote extends BasicRemote {
+
+	public AdvancedRemote(Device device) {
+		
+		super.device = device;
+	}
+
+	public void mute() {
+		
+		System.out.println("Remote: mute");
+		device.setVolume(0);
+	}
+	
+}
